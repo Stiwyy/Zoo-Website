dcbdaf084d7d55ef1def7af27979ef44257b4d56
Andrin Renggli <andrin.renggli@lernende.bbw.ch>
2024-03-27 13:29:51 +0100

    Reduced Start-Animation cooldown by 200ms Added Text to Flughoernchen

--- JS/startAnimationTest.js
+++ JS/startAnimationTest.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     var animationDiv = document.querySelector(".animation");
     setTimeout(function() {
         animationDiv.classList.add("animate");
-    }, 500); // Starten der Animation nach 1 Sekunde
+    }, 300); // Starten der Animation nach 1 Sekunde
 });
