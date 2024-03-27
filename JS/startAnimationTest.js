431c6812fecf7f42e2109454d21abf027afe2b72
Andrin Renggli <andrin.renggli@lernende.bbw.ch>
2024-03-27 14:58:56 +0100

    Fixed Start animation

--- JS/startAnimationTest.js
+++ JS/startAnimationTest.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     var animationDiv = document.querySelector(".animation");
     setTimeout(function() {
         animationDiv.classList.add("animate");
-    }, 300);
+    }, 200);
 });
